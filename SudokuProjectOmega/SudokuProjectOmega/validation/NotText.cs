7a922af1f7e6b2023269106a4e4b1ec95ace4629
uri1sovran <uri1sovran@gmail.com>
2022-02-04 17:13:24 +0200

    added new text exception

---
+++ SudokuProjectOmega/SudokuProjectOmega/validation/NotText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuProjectOmega
+{
+    public class NotText : SudokuException
+    {
+        public NotText() : 
+            base("Not a text file")
+        { 
+        }
+    }
+}
